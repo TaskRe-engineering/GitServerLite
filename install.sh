7353e349053bcd2833350a7ec013f1cea5c05025
Kier von Konigslow <kvonkonigslow@gmail.com>
2023-02-07 23:42:14 -0500

    Refactored so that docker compose is assumed, with branches mapping to docker compose environments

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ DESTINATION=$1
 git init --bare $DESTINATION
 GIT_DIR=$DESTINATION
 
-DEPLOY_DIR="$GIT_DIR/deploy"
+DEPLOY_DIR="$GIT_DIR/deploy_files"
 echo "Creating deploy directory in destination $DEPLOY_DIR/."
-mkdir "$DEPLOY_DIR/"
+mkdir -p "$DEPLOY_DIR/"
 
 CONTAINER_DIR="$GIT_DIR/containers"
 echo "Creating container directory in destination $CONTAINER_DIR/."
@@ -19,5 +19,5 @@ chmod a+x "$CONTAINER_DIR/."
 
 HOOKS_DIR="$GIT_DIR/hooks"
 echo "Setting up git hooks in destination $HOOKS_DIR/."
-cp -rf "hooks/." "$HOOKS_DIR/"
-chmod a+x "$HOOKS_DIR/."
+cp -rf "hooks/post_receive" "$HOOKS_DIR/"
+chmod a+x "$HOOKS_DIR/post_receive"
