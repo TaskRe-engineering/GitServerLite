febadd802ce5e0539fdd15f30cbf9bdad3cf85db
Kier von Konigslow <kvonkonigslow@gmail.com>
2023-02-12 16:56:25 -0500

    Improved readibility and usage

--- install.sh
+++ install.sh
@@ -5,11 +5,7 @@
 
 DESTINATION=$1
 
-DEPLOY_BRANCHES_NAME="deploy_branches"
-DEPLOY_FILES_NAME="deploy_files"
-
 echo "Initializing server at location $DESTINATION/"
-
 git init --bare "$DESTINATION"
 GIT_DIR=$DESTINATION
 
@@ -17,21 +13,22 @@ cp -f "src/gitserverlite" $GIT_DIR/
 chmod a+x "$GIT_DIR/gitserverlite"
 
 MAIN_DIR="$GIT_DIR/.gitserverlite"
-mkdir -p $MAIN_DIR
-echo "DEPLOY_BRANCHES_DIR=$DEPLOY_BRANCHES_NAME" > "$MAIN_DIR/locations"
-echo "DEPLOY_FILES_DIR=$DEPLOY_FILES_NAME" >> "$MAIN_DIR/locations"
-cp -f "src/matchanddeploy" $MAIN_DIR/
+mkdir -p "$MAIN_DIR"
+
+VERSION=$(git describe --tags)
+echo -e "VERSION=\"$VERSION\"" > "$MAIN_DIR/env"
+
+cp -f "src/matchanddeploy" "$MAIN_DIR/"
 chmod a+x "$MAIN_DIR/matchanddeploy"
 
-DEPLOY_FILES_DIR="$GIT_DIR/$DEPLOY_FILES_NAME"
-echo "Creating deploy directory in destination $DEPLOY_FILES_DIR/"
-mkdir -p "$DEPLOY_FILES_DIR/"
-
-DEPLOY_BRANCHES_DIR="$GIT_DIR/$DEPLOY_BRANCHES_NAME"
-echo "Creating container directory in destination $DEPLOY_BRANCHES_DIR/"
-mkdir -p "$DEPLOY_BRANCHES_DIR/"
+DEPLOY_DIR="$GIT_DIR/deploy"
+mkdir -p "$DEPLOY_DIR"
+echo -e "DEPLOY_DIR=\"deploy\"" >> "$MAIN_DIR/env"
 
 HOOKS_DIR="$GIT_DIR/hooks"
 echo "Setting up git hooks in destination $HOOKS_DIR/"
-cp -rf "hooks/post-receive" "$HOOKS_DIR/"
+cp -f "src/post-receive" "$HOOKS_DIR/"
 chmod a+x "$HOOKS_DIR/post-receive"
+
+DEPLOY_BRANCHES_DIR="$MAIN_DIR/deploy_branches"
+mkdir -p "$DEPLOY_BRANCHES_DIR"
